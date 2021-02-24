6a53a67f579d37faa460f01466c223ffcd1c2360
Dara-Ekanth <daraekanth3@gmail.com>
2021-02-24 22:52:10 +0530

    Models created.

--- app_server/models.py
+++ app_server/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Object(models.Model):
+    ObjectID = models.IntegerField()
+    ObjectName = models.CharField(max_length=50)
+    ObjectParent = models.CharField(max_length=50)
+    IsTop = models.IntegerField(null=True)
+    Handle_1 = models.IntegerField()
+    Handle_2 = models.IntegerField()
+    Handle_3 = models.IntegerField()
+    ObjectRevision = models.IntegerField()
+
+    def __str__(self):
+        return str(self.ObjectName)+str(self.ObjectParent)
+class Param(models.Model):
+    ParamID = models.IntegerField()
+    ObjectID = models.ForeignKey(Object,on_delete=models.CASCADE)
+    ParamName = models.CharField(max_length=50)
+    ParamValue = models.IntegerField()
+    ParamCode = models.IntegerField()
+
+    def __str__(self):
+        return str(self.ParamName)+str(self.ParamValue)
