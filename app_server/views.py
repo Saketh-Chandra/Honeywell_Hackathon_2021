e84dda351f06d132d83896bd52b1ab3c233674c8
Saketh Chandra <b.sakethchandra9@gmail.com>
2021-02-26 03:46:15 +0530

    bug was resoled!

--- app_server/views.py
+++ app_server/views.py
@@ -7,10 +7,12 @@ from .models import *
 
 def home(request):
     object_form = ObjectForm()
-    if request.method == 'GET':
-        h1 = request.GET['Handle_1']
-        h2 = request.GET['Handle_2']
-        h3 = request.GET['Handle_3']
+    pdata = None
+    if request.method == 'POST':
+        object_form = ObjectForm()
+        h1 = request.POST.get('Handle_1')
+        h2 = request.POST.get('Handle_2')
+        h3 = request.POST.get('Handle_3')
         print(h1, h2, h3)
 
         Odata = Object.objects.get(Handle_1=h1, Handle_2=h2, Handle_3=h3)
@@ -19,5 +21,5 @@ def home(request):
         # print(Odata, pdata, "Hello World-1111111", sep='\n \n')
         # for i in pdata:
         #     print(i.ParamValue)
-    context = {'object_form': object_form,'pdata':pdata}
+    context = {'object_form': object_form, 'pdata': pdata}
     return render(request, 'app_server/home_page.html', context=context)
